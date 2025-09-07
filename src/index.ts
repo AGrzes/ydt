e7d69eff1da8a74de2728caa9ecabb7b55aff314
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-07 16:27:36 +0200

    Use templating engine

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ import { dirname } from 'path'
 import yaml from 'yaml'
 import { FileGenerationConfig } from './model.js'
 import { createResolveTemplate } from './resolve-template.js'
+import { createTemplatingEngine } from './templating-engine.js'
 import { createWriteOutput } from './write-output.js'
 
 const handlebars = Handlebars.create()
 const writeOutput = createWriteOutput(writeFile)
 const resolveTemplate = createResolveTemplate((path) => readFile(path, 'utf-8'))
+const templateEngine = createTemplatingEngine()
 async function generate(configPath: string, context: Record<string, any> = {}): Promise<void> {
-  const configText = handlebars.compile(await readFile(configPath, 'utf-8'))(context)
+  const configText = templateEngine(await readFile(configPath, 'utf-8'), context)
   const config: FileGenerationConfig[] = yaml.parse(configText) || []
   const resolveConfig = {
     basePath: dirname(configPath),
@@ -18,8 +20,7 @@ async function generate(configPath: string, context: Record<string, any> = {}): 
   await Promise.all(
     config.map(async (fileConfig: FileGenerationConfig) => {
       const template = await resolveTemplate(fileConfig.template, resolveConfig)
-      const compiled = handlebars.compile(template)
-      const output = compiled({ ...context, ...fileConfig.context })
+      const output = templateEngine(template, { ...context, ...fileConfig.context })
       await writeOutput(fileConfig.path, output, fileConfig.mergeMode)
     })
   )
