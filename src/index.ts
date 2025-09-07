64a4cb9d762f40c82709b63ea12d60b0f59b777b
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-07 11:53:05 +0200

    Use resolve-template

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import { readFile, writeFile } from 'fs/promises'
 import Handlebars from 'handlebars'
+import { dirname } from 'path'
 import yaml from 'yaml'
 import { FileGenerationConfig } from './model.js'
+import { createResolveTemplate } from './resolve-template.js'
 import { createWriteOutput } from './write-output.js'
 
 const handlebars = Handlebars.create()
 const writeOutput = createWriteOutput(writeFile)
-
+const resolveTemplate = createResolveTemplate((path) => readFile(path, 'utf-8'))
 async function generate(configPath: string, context: Record<string, any> = {}): Promise<void> {
   const configText = handlebars.compile(await readFile(configPath, 'utf-8'))(context)
   const config: FileGenerationConfig[] = yaml.parse(configText) || []
+  const resolveConfig = {
+    basePath: dirname(configPath),
+  }
   await Promise.all(
     config.map(async (fileConfig: FileGenerationConfig) => {
-      const template =
-        'content' in fileConfig.template
-          ? fileConfig.template.content
-          : await readFile(fileConfig.template.path, 'utf-8')
+      const template = await resolveTemplate(fileConfig.template, resolveConfig)
       const compiled = handlebars.compile(template)
       const output = compiled({ ...context, ...fileConfig.context })
       await writeOutput(fileConfig.path, output, fileConfig.mergeMode)
