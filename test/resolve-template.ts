3eb09b35873144d59b8a21633ca193fb3fedd831
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-07 11:50:54 +0200

    All tests for resolve template

--- test/resolve-template.ts
+++ test/resolve-template.ts
@@ -14,5 +14,19 @@ describe('ytd', () => {
       expect(content).to.equal('inline template')
       expect(lookupFile).not.to.have.been.called
     })
+    it('should resolve file template without basePath', async () => {
+      const lookupFile = sinon.stub().resolves('file template content')
+      const resolveTemplate = createResolveTemplate(lookupFile)
+      const content = await resolveTemplate({ path: 'template.txt' })
+      expect(content).to.equal('file template content')
+      expect(lookupFile).to.have.been.calledOnceWithExactly('template.txt')
+    })
+    it('should resolve file template with basePath', async () => {
+      const lookupFile = sinon.stub().resolves('file template content with basePath')
+      const resolveTemplate = createResolveTemplate(lookupFile)
+      const content = await resolveTemplate({ path: 'template.txt' }, { basePath: '/base/dir' })
+      expect(content).to.equal('file template content with basePath')
+      expect(lookupFile).to.have.been.calledOnceWithExactly('/base/dir/template.txt')
+    })
   })
 })
