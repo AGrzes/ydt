f2952388c0da7ee0492f4a82de7f42d60db002db
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-10 08:04:37 +0200

    Program entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+/* v8 ignore start */
 import { readFile, writeFile } from 'fs/promises'
-import yaml from 'yaml'
+import { createProgram } from './command.js'
 import { createFlow } from './flow.js'
 import { createResolveTemplate } from './resolve-template.js'
 import { createTemplatingEngine } from './templating-engine.js'
@@ -10,6 +11,6 @@ const resolveTemplate = createResolveTemplate((path) => readFile(path, 'utf-8'))
 const templateEngine = createTemplatingEngine()
 const generate = createFlow(templateEngine, resolveTemplate, writeOutput)
 
-const input = Buffer.concat(await process.stdin.toArray()).toString('utf-8')
+const program = createProgram(generate)
 
-await generate(process.argv[2], yaml.parse(input))
+await program.parseAsync(process.argv)
