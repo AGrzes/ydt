7b3fe1d5a7e1fe30591dc7fefabe0be5ab4e031f
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-10 08:13:04 +0200

    Program flow

---
+++ src/program.ts
@@ -0,0 +1,16 @@
+import { Command } from 'commander'
+import yaml from 'yaml'
+import { Flow } from './model.js'
+
+export function createProgram(generate: Flow) {
+  const program = new Command()
+  program
+    .name('ydt')
+    .description('Multi file templating tool')
+    .argument('<configPath>', 'Path to configuration file')
+    .action(async (config) => {
+      const input = Buffer.concat(await process.stdin.toArray()).toString('utf-8')
+      await generate(config, yaml.parse(input))
+    })
+  return program
+}
---
+++ test/program.ts
@@ -0,0 +1,37 @@
+import * as chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import { createProgram } from '../src/program.js'
+
+const { expect } = chai.use(sinonChai).use(chaiAsPromised)
+
+describe('ytd', () => {
+  describe('program', () => {
+    it('should run program with given config and context', async () => {
+      const generate = sinon.stub().resolves()
+      const program = createProgram(generate)
+      program.exitOverride(() => {
+        throw new Error('process.exit called')
+      })
+      const configPath = 'path/to/config.yaml'
+      const context = { name: 'World' }
+      const input = JSON.stringify(context)
+      const stdin = sinon.stub(process.stdin, 'toArray').resolves([Buffer.from(input)])
+      await program.parseAsync([configPath], { from: 'user' })
+      expect(generate).to.have.been.calledOnceWithExactly(configPath, context)
+      stdin.restore()
+    })
+    it('should throw error when no config path is provided', async () => {
+      const generate = sinon.stub().resolves()
+      const program = createProgram(generate)
+      program.exitOverride(() => {
+        throw new Error('process.exit called')
+      })
+      const stdin = sinon.stub(process.stdin, 'toArray').resolves([Buffer.from('{}')])
+      await expect(program.parseAsync([], { from: 'user' })).to.eventually.be.rejectedWith('process.exit called')
+      expect(generate).to.not.have.been.called
+      stdin.restore()
+    })
+  })
+})
