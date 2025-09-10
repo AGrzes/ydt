5cf3b93f039a879976db67419491698d973bc36c
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-10 07:52:31 +0200

    Use flow

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
 import { readFile, writeFile } from 'fs/promises'
-import Handlebars from 'handlebars'
-import { dirname } from 'path'
 import yaml from 'yaml'
-import { FileGenerationConfig } from './model.js'
+import { createFlow } from './flow.js'
 import { createResolveTemplate } from './resolve-template.js'
 import { createTemplatingEngine } from './templating-engine.js'
 import { createWriteOutput } from './write-output.js'
 
-const handlebars = Handlebars.create()
 const writeOutput = createWriteOutput(writeFile)
 const resolveTemplate = createResolveTemplate((path) => readFile(path, 'utf-8'))
 const templateEngine = createTemplatingEngine()
-async function generate(configPath: string, context: Record<string, any> = {}): Promise<void> {
-  const configText = templateEngine(await readFile(configPath, 'utf-8'), context)
-  const config: FileGenerationConfig[] = yaml.parse(configText) || []
-  const resolveConfig = {
-    basePath: dirname(configPath),
-  }
-  await Promise.all(
-    config.map(async (fileConfig: FileGenerationConfig) => {
-      const template = await resolveTemplate(fileConfig.template, resolveConfig)
-      const output = templateEngine(template, { ...context, ...fileConfig.context })
-      await writeOutput(fileConfig.path, output, fileConfig.mergeMode)
-    })
-  )
-}
+const generate = createFlow(templateEngine, resolveTemplate, writeOutput)
 
 const input = Buffer.concat(await process.stdin.toArray()).toString('utf-8')
 
