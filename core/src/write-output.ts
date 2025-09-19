5d32b591caae45e6be5a218d4e34d6a81809d601
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-19 07:48:50 +0200

    Create directory

--- core/src/write-output.ts
+++ core/src/write-output.ts
@@ -1,19 +1,21 @@
 import fs from 'fs/promises'
+import { dirname } from 'path'
 import { MergeMode, WriteOutput } from './model.js'
 
-export function createWriteOutput(writeFile: typeof fs.writeFile): WriteOutput {
+export function createWriteOutput(_fs: Pick<typeof fs, 'mkdir' | 'writeFile'>): WriteOutput {
   return async function writeOutput(path: string, content: string, mergeMode: MergeMode): Promise<void> {
+    await _fs.mkdir(dirname(path), { recursive: true })
     switch (mergeMode) {
       case 'skip':
         try {
-          await writeFile(path, content, { flag: 'wx' })
+          await _fs.writeFile(path, content, { flag: 'wx' })
         } catch (e: any) {
           if (e.code !== 'EEXIST') throw e
         }
         break
       case 'overwrite':
       default:
-        await writeFile(path, content)
+        await _fs.writeFile(path, content)
         break
     }
   }
--- core/test/write-output.ts
+++ core/test/write-output.ts
@@ -9,29 +9,34 @@ describe('ytd', () => {
   describe('write-output', () => {
     it('should write new file with skip mode', async () => {
       const writeFile = sinon.stub().resolves()
-      await createWriteOutput(writeFile)('path/to/file.txt', 'file content', 'skip')
+      const mkdir = sinon.stub().resolves()
+      await createWriteOutput({ writeFile, mkdir })('path/to/file.txt', 'file content', 'skip')
       expect(writeFile).to.have.been.calledOnceWithExactly('path/to/file.txt', 'file content', { flag: 'wx' })
     })
     it('should not overwrite existing file with skip mode', async () => {
       const writeFile = sinon.stub().rejects({ code: 'EEXIST' })
-      await expect(createWriteOutput(writeFile)('path/to/file.txt', 'file content', 'skip')).to.be.fulfilled
+      const mkdir = sinon.stub().resolves()
+      await expect(createWriteOutput({ writeFile, mkdir })('path/to/file.txt', 'file content', 'skip')).to.be.fulfilled
       expect(writeFile).to.have.been.calledOnceWithExactly('path/to/file.txt', 'file content', { flag: 'wx' })
     })
     it('should propagate other errors with skip mode', async () => {
       const writeFile = sinon.stub().rejects(new Error('Some error'))
-      await expect(createWriteOutput(writeFile)('path/to/file.txt', 'file content', 'skip')).to.be.rejectedWith(
-        'Some error'
-      )
+      const mkdir = sinon.stub().resolves()
+      await expect(
+        createWriteOutput({ writeFile, mkdir })('path/to/file.txt', 'file content', 'skip')
+      ).to.be.rejectedWith('Some error')
       expect(writeFile).to.have.been.calledOnceWithExactly('path/to/file.txt', 'file content', { flag: 'wx' })
     })
     it('should overwrite existing file with overwrite mode', async () => {
       const writeFile = sinon.stub().resolves()
-      await createWriteOutput(writeFile)('path/to/file.txt', 'file content', 'overwrite')
+      const mkdir = sinon.stub().resolves()
+      await createWriteOutput({ writeFile, mkdir })('path/to/file.txt', 'file content', 'overwrite')
       expect(writeFile).to.have.been.calledOnceWithExactly('path/to/file.txt', 'file content')
     })
     it('should overwrite existing file with default mode', async () => {
       const writeFile = sinon.stub().resolves()
-      await createWriteOutput(writeFile)('path/to/file.txt', 'file content', 'unknown' as any)
+      const mkdir = sinon.stub().resolves()
+      await createWriteOutput({ writeFile, mkdir })('path/to/file.txt', 'file content', 'unknown' as any)
       expect(writeFile).to.have.been.calledOnceWithExactly('path/to/file.txt', 'file content')
     })
   })
