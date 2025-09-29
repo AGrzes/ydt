63f90e9e0eac05e0ba51b9eb62844444b8cdf8e7
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-09-29 22:38:46 +0200

    Dynamic helpers

--- cli/src/program.ts
+++ cli/src/program.ts
@@ -1,15 +1,9 @@
-import {
-  BUILTIN_HELPERS,
-  createFlow,
-  createResolveTemplate,
-  createTemplatingEngine,
-  createWriteOutput,
-} from '@agrzes/ydt'
+import { createFlow, createResolveTemplate, createTemplatingEngine, createWriteOutput } from '@agrzes/ydt'
 import { Flow } from '@agrzes/ydt/src/model.js'
-import fs, { readFile } from 'fs/promises'
-
 import { Command } from 'commander'
+import fs, { readFile } from 'fs/promises'
 import yaml from 'yaml'
+import { initHelpers } from './helpers.js'
 
 export function createProgram(
   cwo: typeof createWriteOutput,
@@ -17,17 +11,18 @@ export function createProgram(
   cte: typeof createTemplatingEngine,
   cf: typeof createFlow
 ): Command {
-  const writeOutput = cwo(fs)
-  const resolveTemplate = crt((path) => readFile(path, 'utf-8'))
-  const templateEngine = cte(BUILTIN_HELPERS)
-  const generate: Flow = cf(templateEngine, resolveTemplate, writeOutput)
   const program = new Command()
   program
     .name('ydt')
     .description('Multi file templating tool')
     .argument('<configPath>', 'Path to configuration file')
-    .action(async (config) => {
+    .option('-H, --helpers <helpers...>', 'Additional helpers modules')
+    .action(async (config, options) => {
       const input = Buffer.concat(await process.stdin.toArray()).toString('utf-8')
+      const writeOutput = cwo(fs)
+      const resolveTemplate = crt((path) => readFile(path, 'utf-8'))
+      const templateEngine = cte(await initHelpers(options.helpers))
+      const generate: Flow = cf(templateEngine, resolveTemplate, writeOutput)
       await generate(config, yaml.parse(input))
     })
   return program
