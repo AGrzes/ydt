cc4ab2859222c74f45a10b31b6b6d83928c41019
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-10-02 12:24:39 +0200

    No TS at runtime

--- cli/src/helpers.ts
+++ cli/src/helpers.ts
@@ -1,5 +1,7 @@
 import { BUILTIN_HELPERS } from '@agrzes/ydt'
 import fs from 'fs/promises'
+import path from 'path'
+import { cwd } from 'process'
 
 export async function initHelpers(helpers: string[] = []) {
   const result = {
@@ -8,13 +10,13 @@ export async function initHelpers(helpers: string[] = []) {
   for (const helperPath of helpers) {
     const stat = await fs.lstat(helperPath)
     if (stat.isFile()) {
-      const helperModule = await import(helperPath)
+      const helperModule = await import(path.resolve(cwd(), helperPath))
       Object.assign(result, helperModule)
     } else if (stat.isDirectory()) {
       const files = await fs.readdir(helperPath)
       for (const file of files) {
-        if (file.endsWith('.js') || file.endsWith('.ts')) {
-          const helperModule = await import(`${helperPath}/${file}`)
+        if (file.endsWith('.js')) {
+          const helperModule = await import(path.resolve(cwd(), helperPath, file))
           Object.assign(result, helperModule)
         }
       }
